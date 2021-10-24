40d61d8117502bf34fea112ffadb10350c649393
skywing <skywing@users.noreply.github.com>
2021-10-24 19:11:02 -0400

    15 tables and its relationship

--- src/models.py
+++ src/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, \
-        TIMESTAMP, TEXT, DECIMAL, SMALLINT, func, text, Index, Sequence
-from sqlalchemy.orm import relationship, backref, registry
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy import TIMESTAMP, TEXT, DECIMAL, SMALLINT, Index, LargeBinary, BOOLEAN
+from sqlalchemy import func, text, Sequence
+from sqlalchemy.orm import relation, relationship, backref, registry
 from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.schema import MetaData
-from sqlalchemy.sql.sqltypes import ARRAY, PickleType
+from sqlalchemy.sql.sqltypes import ARRAY, Time
+from sqlalchemy.sql.traversals import ColIdentityComparatorStrategy
 
 mapper_registry = registry(metadata=MetaData(schema='public'))
 
@@ -31,6 +33,15 @@ film_actor = Table(
     mapper_registry.metadata,
     Column('actor_id', Integer, ForeignKey('actor.actor_id'), primary_key=True),
     Column('film_id', Integer, ForeignKey('film.film_id'), primary_key=True),
+    Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+)
+
+inventory = Table(
+    'inventory',
+    mapper_registry.metadata,
+    Column('inventory_id', Integer, Sequence('inventory_inventory_id_seq'), primary_key=True),
+    Column('film_id', Integer, ForeignKey('film.film_id')),
+    Column('store_id', Integer, ForeignKey('store.store_id')),
     Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
 )
 
@@ -69,7 +80,7 @@ class Film():
     language = relationship('Language')
     categories = relationship('Category', secondary=film_category, backref='films')
     actors = relationship('Actor', secondary=film_actor, backref='actors')
-
+    stores = relationship('Store', secondary=inventory, backref='stores')
     __table_args__ = (
         Index('ix_film_fulltext', to_tsvector_ix('title', 'description'), postgresql_using='gin'),
     )
@@ -82,7 +93,115 @@ class Category():
     last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
 
 @mapper_registry.mapped
-class Inventory():
-    __tablename__ = 'inventory'
-    inventory_id = Column('inventory_id', Integer, Sequence('inventory_inventory_id_seq', primary_key=True)
-    
+class Store():
+    __tablename__ = 'store'
+    store_id = Column('store_id', Integer, Sequence('store_store_id_seq'), primary_key=True)
+    manager_staff_id = Column('manager_staff_id', ForeignKey('staff.staff_id'), nullable=False)
+    address_id = Column('address_id', ForeignKey('address.address_id'), nullable=False)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    '''
+        The store can only be in one location, so set the relationship to return a single
+        scalar object instead of list.
+    '''
+    address = relationship('Address', uselist=False)
+
+@mapper_registry.mapped
+class Staff():
+    __tablename__ = 'staff'
+    staff_id = Column('staff_id', Integer, Sequence('staff_staff_id_seq'), primary_key=True)
+    first_name = Column('first_name', String(45), nullable=False)
+    last_name = Column('last_name', String(45), nullable=False)
+    address_id = Column('address_id', Integer, ForeignKey('address.address_id'), nullable=False)
+    email = Column('email', String(50))
+    store_id = Column('store_id', Integer, ForeignKey('store.store_id'), nullable=False)
+    username = Column('username', String(16), nullable=False)
+    password = Column('password', String(40))
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    picture = Column('picture', LargeBinary)
+
+    '''
+        This assume the staff can be a manager for multiple store.
+        If it is a a staff can be only the manager for a store, we can set the uselist to False.
+        Example: stores = relationship('Store', foreign_keys='Store.manager_staff_id', uselist=False)
+    '''
+    stores = relationship('Store', foreign_keys='Store.manager_staff_id', backref='manager')
+    address = relationship('Address')
+
+@mapper_registry.mapped
+class Address():
+    __tablename__ = 'address'
+    address_id = Column('address_id', Integer, Sequence('address_address_id_seq'), primary_key=True)
+    address = Column('address', String(50), nullable=False)
+    address2 = Column('address2', String(50))
+    district = Column('district', String(20), nullable=False)
+    city_id = Column('city_id', ForeignKey('city.city_id'), nullable=False)
+    postal_code = Column('postal_code', String(10))
+    phone = Column('phone', String(20), nullable=False)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    city = relationship('City')
+
+@mapper_registry.mapped
+class City():
+    __tablename__ = 'city'
+    city_id = Column('city_id', Integer, Sequence('city_city_id_seq'), primary_key=True)
+    city = Column('city', String(50), nullable=False)
+    country_id = Column('country_id', Integer, ForeignKey('country.country_id'), nullable=False)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    '''
+        Use eager loading so the country is loaded as part of the inner joined query
+    '''
+    country = relationship('Country', lazy='joined', innerjoin=True)
+
+
+@mapper_registry.mapped
+class Country():
+    __tablename__ = 'country'
+    country_id = Column('country_id', Integer, Sequence('country_country_id_seq'), primary_key=True)
+    country = Column('country', String(50), nullable=False)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+
+@mapper_registry.mapped
+class Rental():
+    __tablename__ = 'rental'
+    rental_id = Column('rental_id', Integer, Sequence('rental_rental_id_seq'), primary_key=True)
+    rental_date = Column('rental_date', TIMESTAMP, nullable=False)
+    inventory_id = Column('inventory_id', Integer, ForeignKey('inventory.inventory_id'), nullable=False)
+    customer_id = Column('customer_id', Integer, ForeignKey('customer.customer_id'), nullable=False)
+    return_date = Column('return_date', TIMESTAMP)
+    staff_id = Column('staff_id', Integer, ForeignKey('staff.staff_id'), nullable=False)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+
+    film = relationship('Film', secondary=inventory, uselist=False)
+    staff = relationship('Staff', lazy='joined', innerjoin=True)
+    customer = relationship('Customer', lazy='joined', innerjoin=True)
+
+@mapper_registry.mapped
+class Customer():
+    __tablename__ = 'customer'
+    customer_id = Column('customer_id', Integer, Sequence('customer_customer_id_seq'), primary_key=True)
+    store_id = Column('store_id', Integer, ForeignKey('store.store_id'), nullable=False)
+    first_name = Column('first_name', String(45), nullable=False)
+    last_name = Column('last_name', String(45), nullable=False)
+    email = Column('email', String(50))
+    address_id = Column('address_id', Integer, ForeignKey('address.address_id'), nullable=False)
+    activebool = Column('activebool', BOOLEAN, default=True, nullable=False)
+    create_date = Column('create_date', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    last_update = Column('last_update', TIMESTAMP, nullable=False, default=datetime.utcnow)
+    active = Column('active', Integer)
+
+    store = relationship('Store')
+    address = relationship('Address')
+
+@mapper_registry.mapped
+class Payment():
+    __tablename__ = 'payment'
+    payment_id = Column('payment_id', Integer, Sequence('payment_payment_id_seq'), primary_key=True)
+    customer_id = Column('customer_id', Integer, ForeignKey('customer.customer_id'), nullable=False)
+    staff_id = Column('staff_id', Integer, ForeignKey('staff.staff_id'), nullable=False)
+    rental_id = Column('rental_id', Integer, ForeignKey('rental.rental_id'), nullable=False)
+    amount = Column('amount', DECIMAL(5,2), nullable=False)
+    payment_date = Column('payment_date', TIMESTAMP, nullable=False)
+
+    customer = relationship('Customer')
+    staff = relationship('Staff')
+    rental = relationship('Rental', backref='payment')
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,17 @@
 
+from datetime import datetime
+from decimal import Decimal
 from typing import Any, List
 from operator import attrgetter
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, func
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
-from models import Category, Film, Language, Actor, film_actor
+from models import Category, Film, Language, Actor, Store, Staff
+from models import Rental, Payment
+from models import film_actor, film_category, inventory
 import pytest
 
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name, pointless-string-statement
 
 @pytest.fixture(scope='class')
 def engine():
@@ -44,9 +48,73 @@ def test_film_category_model(session: Session):
     assert len(category.films) == 74
 
 def test_film_actor_association_model(session: Session):
-    stmt = select(Film.title, Actor.first_name, Actor.last_name).join(Film.actors).where(Actor.actor_id == 107)
+    stmt = select(Film.film_id, Film.title, Actor.first_name, Actor.last_name).join(Film.actors).where(Actor.actor_id == 107)
     films : Any = session.execute(stmt).all()
     assert len(films) == 42
-    film = list(filter(lambda f: f.title == 'Chamber Italian', films))
-    print(film)
+    film : Any = list(filter(lambda f: f.title == 'Chamber Italian', films))
+    assert len(film) == 1
+    assert film[0].film_id == 133
 
+def test_film_inventory_store_model(session: Session):
+    stmt = select(Film.film_id, Film.title, func.count(), Store.store_id)\
+            .join(Film.stores)\
+            .group_by(Film.film_id, Store.store_id)\
+            .where(Store.store_id == 1)
+    films_at_store : Any = session.execute(stmt).all()
+    assert len(films_at_store) == 759
+    film_inventory = list(filter(lambda fc: fc.film_id == 253, films_at_store))
+    assert len(film_inventory) == 1
+    assert film_inventory[0].count == 4
+
+def test_staff_store_address_model(session: Session):
+    stmt = select(Staff).where(Staff.staff_id == 2)
+    staff = session.execute(stmt).scalar_one_or_none()
+    assert staff is not None
+    assert len(staff.stores) == 1
+    staff_store : Store = staff.stores[0]
+    assert staff_store.store_id == 2
+    assert staff_store.address_id == 2
+    assert staff_store.manager is not None
+    assert staff_store.manager.first_name == 'Jon'
+    assert staff_store.manager.email == 'Jon.Stephens@sakilastaff.com'
+    
+    assert staff.address.address_id == 4
+    assert staff.address.city.city == 'Woodridge'
+
+
+def test_store_address_model(session: Session):
+    stmt = select(Store).where(Store.store_id == 1)
+    store = session.execute(stmt).scalar_one_or_none()
+    assert store is not None
+    assert store.address is not None
+    assert store.address.address == '47 MySakila Drive'
+    '''
+        The city is lazy load by default so accessing the city
+        will trigger the session to execute query to load the city
+        by the given city_id
+    '''
+    assert store.address.city.city == 'Lethbridge'
+    '''
+        The country is configured to use eager loading. When the above city is accessed
+        and the country is loaded as part of the city loading with inner join.
+        Example: country = relationship('Country', lazy='joined', innerjoin=True, uselist=False)
+    '''
+    assert store.address.city.country.country == 'Canada'
+
+def test_rental_customer_model(session: Session):
+    stmt = select(Rental).where(Rental.rental_id == 2)
+    rental = session.execute(stmt).scalar_one_or_none()
+    assert rental is not None
+    assert rental.rental_date >= datetime(2005, 5, 24)
+    assert rental.staff.email == 'Mike.Hillyer@sakilastaff.com'
+    assert rental.customer.email == 'tommy.collazo@sakilacustomer.org'
+    assert rental.film.title == 'Freaky Pocus'
+
+def test_payment_rental_customer_staff(session: Session):
+    stmt = select(Payment).where(Payment.payment_id == 17503)
+    payment = session.execute(stmt).scalar_one_or_none()
+    assert payment is not None
+    assert payment.amount == pytest.approx(Decimal(7.99))
+    assert payment.payment_date.date() >= datetime(2007, 2, 15).date()
+    assert payment.customer.email == 'peter.menard@sakilacustomer.org'
+    assert payment.staff.email == 'Jon.Stephens@sakilastaff.com'
